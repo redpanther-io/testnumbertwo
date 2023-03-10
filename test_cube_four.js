88ee0d79393dd8562c8d51887f4377daa13fe571
redpanther-io <103533343+redpanther-io@users.noreply.github.com>
2023-03-10 11:36:10 -0600

    Add files via upload

--- test_cube_four.js
+++ test_cube_four.js
@@ -11,8 +11,8 @@ var CONTAINER_ID = 'v3d-container';
     var params = v3d.AppUtils.getPageParams();
 
     var PUZZLES_DIR = '/puzzles/';
-    var logicURL = params.logic ? params.logic : '__LOGIC__visual_logic.js'.replace('__LOGIC__', '');
-    var sceneURL = params.load ? params.load : '__URL__test_cube_four.gltf'.replace('__URL__', '');
+    var logicURL = params.logic ? params.logic : 'https://cdn.jsdelivr.net/gh/redpanther-io/testnumbertwo/visual_logic.js';
+    var sceneURL = params.load ? params.load : 'https://cdn.jsdelivr.net/gh/redpanther-io/testnumbertwo/test_cube_four.gltf';
     if (!sceneURL) {
         console.log('No scene URL specified');
         return;
